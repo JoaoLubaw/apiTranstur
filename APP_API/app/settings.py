a331027f422be81befe43b779ac645c924217481
João Lubawaski <jlubawaski@gmail.com>
2023-11-25 17:20:47 -0300

    now

--- APP_API/app/settings.py
+++ APP_API/app/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,9 +126,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Diretório no qual o collectstatic irá coletar os arquivos estáticos.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'medias')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
