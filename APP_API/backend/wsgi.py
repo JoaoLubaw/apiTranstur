0701614917eb16870d5e3cac8709cf940143eed4
João Lubawaski <jlubawaski@gmail.com>
2023-11-26 19:02:58 -0300

    wsgi

--- APP_API/backend/wsgi.py
+++ APP_API/backend/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles_build', 'static'))
 
 app = application
