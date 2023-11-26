56ed4f4fc79db0a2c9ae10e7baacb8a586dfa6ad
João Lubawaski <jlubawaski@gmail.com>
2023-11-26 19:36:07 -0300

    bug

--- APP_API/backend/wsgi.py
+++ APP_API/backend/wsgi.py
@@ -26,7 +26,7 @@ if BASE_DIR is None:
 application = get_wsgi_application()
 
 # Configure o WhiteNoise após a configuração da aplicação WSGI
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles_build', 'static'))
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
 
 # Atribua a aplicação WSGI configurada ao 'app'
 app = application
