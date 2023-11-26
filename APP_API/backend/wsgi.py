27c63afb8d165f11a9631634b06d125f4f886ff9
João Lubawaski <jlubawaski@gmail.com>
2023-11-26 20:52:56 -0300

    Pai Nosso que estais nos Céus, santificado seja o vosso Nome, venha a nós o vosso Reino, seja feita a vossa vontade assim na terra como no Céu. O pão nosso de cada dia nos dai hoje, perdoai-nos as nossas ofensas assim como nós perdoamos a quem nos tem ofendido, e não nos deixeis cair em tentação, mas livrai-nos do Mal, amém

--- APP_API/backend/wsgi.py
+++ APP_API/backend/wsgi.py
@@ -1,13 +1,11 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
+# APP_API/backend/wsgi.py
 
 import os
+import sys
+
+# Adiciona o diretório raiz do projeto ao caminho de pesquisa de módulos do Python
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
