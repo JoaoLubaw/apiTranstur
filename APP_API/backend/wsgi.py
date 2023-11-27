95c01a0df7900d0cbf1508c2d17efe13895a287d
João Lubawaski <jlubawaski@gmail.com>
2023-11-26 21:01:25 -0300

    Crux Sacra Sit Mihi Lux. Non Draco Sit Mihi Dux. Vade Retro Satana! Nunquam Suade Mihi Vana. Sunt Mala Quae Libas. Ipse Venena Bibas.

--- APP_API/backend/wsgi.py
+++ APP_API/backend/wsgi.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 # Adiciona o diretório raiz do projeto ao caminho de pesquisa de módulos do Python
-sys.path.append(os.path.join(os.path.dirname(__file__), 'apiTT'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'APP_API'))
 
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
